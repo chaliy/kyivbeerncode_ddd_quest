d888a475dcb22172a896e2fde6e4703b2a6369c0
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2012-01-31 16:54:11 +0200

    Added NUnit-based infrastructure for writing GWT specs

---
+++ src/Tests/Domain/Meetings/When_register_already_registered_attendie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using FluentAssertions;
+
+namespace KyivBeerNCode.Domain.Meetings
+{
+	[Exceptional, Context]
+    public class When_register_already_registered_attendie : Specification
+    {
+        Meeting _meeting;
+
+		public When_register_already_registered_attendie()
+		{
+			Expect<DomainException>();
+		}
+
+		protected override void Given()
+		{
+			_meeting = new Meeting("DDD Quest Meeting");
+			_meeting.RegisterAttendie("Ivan Korneliuk");
+		}
+
+		protected override void When()
+		{
+			_meeting.RegisterAttendie("Ivan Korneliuk");
+		}
+
+        [Then]
+        public void An_exception_should_be_thrown()
+        {
+        	var exception = Thrown<DomainException>();
+
+			// assert any exception details here
+			exception.Message.Should().NotBeEmpty();
+        }
+    }
+}
